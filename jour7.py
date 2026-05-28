0a9b944acf8da9b75388f843268f6e5bfc7ab202
Student <etienne.vanwesel@gmail.com>
2026-05-28 09:27:32 +0200

    maj jour 4 et jour 7

---
+++ jour7.py
@@ -0,0 +1,16 @@
+complexité
+
+Vert:
+O(1)
+O(log n)--> à chaque operation on divise par 2 le nombre d'operation par rapport aux nobres d'élément qui reste
+exemple logaritme 
+le balancement d'un red black tree -->log n
+jaune:
+O(n) boucle for  --> lineaire , n est la taille du tableau
+orange:
+O(n log n)-->linearithmic
+
+rouge:
+O(n^2) 2 boucle for
+O(2^n)
+O(n!) --> recursive qui s'appelle 2 fois (exemple fibonaci)
--- jours4.py
+++ jours4.py
@@ -12,17 +12,18 @@
 # with -> bonne façon de faire car il va gérer fermeture dans tous les cas
 # ->ouverture fichier, ou DB
 # peut mettre un try except dans un with
-# 
+ 
 # page 115 erreur dans le slide, il n'y aura pas d'erreur dans le 3eme cas
-# 
+
 #  peut mettre un tuple comme type d'except : p 118
 # traceback -> trace les erreurs jusqu'à l'origine
-# traceback.print_exc() -> pour debug c'est bien et pour prod faut faire attention à ce qu'on envoie
+# traceback.print_exc() -> pour debug c'est bien et 
+# pour prod faut faire attention à ce qu'on envoie
 
-# Check les ORM -> securiser injection url, corerule github, modesecurity-crs (owasp)
+# Check les ORM -> securiser injection url, corerule github, 
+# modesecurity-crs (owasp)
 # sanitize la string, éviter la concaténation
 
 # dbeaver -> to check pour semaine prochaine
 # ou DataGrip
 # vs code plugin -> database client JDBC
-
