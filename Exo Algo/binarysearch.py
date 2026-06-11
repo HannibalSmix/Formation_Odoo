ad2bdb98ed7841209165aaa0a9473d3197e7d2b9
Student <etienne.vanwesel@gmail.com>
2026-06-11 15:14:16 +0200

    two pointers

--- Exo Algo/binarysearch.py
+++ Exo Algo/binarysearch.py
@@ -12,3 +12,23 @@ def searchInsert(self, nums: List[int], target: int) -> int:
         else:
             right=mid-1
     return left
+
+
+
+# two pointers :
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         left = 0
+#         right = len(s) - 1
+#         while left < right:
+#             if not s[left].isalnum():
+#                 left += 1
+#             elif not s[right].isalnum():
+#                 right -= 1
+#             elif s[left].lower() != s[right].lower():
+#                 return False
+#             else:
+#                 left += 1
+#                 right -= 1
+#         return True
